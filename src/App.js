9b7fbdd547e8577e5f6c4aa3410bbeaee2d631b9
midhunrajk <midhunrajk009@gmail.com>
2023-02-21 23:20:16 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React  from "react";
 import { useState } from "react";
 import "./App.css"
 function App(){
-    const apiKey='faaea469f097f09e6653b752c108dbd7';
     const [weatherData,setWeatherData]=useState([{}])
     const [city,setCity]=useState("")
+    const apiKey=process.env.API_KEY;
+    const apiUrl=`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
     const getWeather=(event)=>{
         if(event.key==="Enter"){
-            fetch('https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apikey}') 
+            fetch(apiUrl)
             .then(response=>response.json())
-            .then(data=>{setWeatherData(data)})
+            .then(data=>{
+                setWeatherData(data) 
+                setCity("")
+            })
         }
     }
     return(
@@ -21,6 +25,22 @@ function App(){
                 value={city}
                 onKeyPress={getWeather}
             />
+            {typeof weatherData.main==='undefined'?(
+                <div>
+                    <p>Welcome to Weather App! Enter in a city to get the weather of. </p>
+                </div>
+            ):(
+                <div className="weather-data">
+                    <p className="city">{weatherData.name}</p>
+                    <p className="temp">{Math.round(weatherData.main.temp)}oF</p>
+                    <p className="weather">{weatherData.weather[0].main}</p>
+                </div>
+            )}
+            {weatherData.cod==="404"?(
+                <p>city not found</p>
+            ):(
+                <></>
+            )}
         </div>
     )
 }
